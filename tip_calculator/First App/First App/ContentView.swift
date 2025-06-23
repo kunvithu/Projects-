38ea71809454a6c70b28e4d3a092e38edaaf0b52
Aden <cambosoccer25@gmail.com>
2025-06-22 17:36:39 -0700

    transfering xcode files into dir

---
+++ tip_calculator/First App/First App/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  First App
+//
+//  Created by Aden Kunvithu Svay on 8/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var total = ""
+    @State var tipPercent = 15.0
+    var body: some View {
+        HStack {
+            Image(systemName: "dollarsign.circle.fill")
+                .imageScale(.small)
+                .foregroundStyle(.tint)
+                .font(.title)
+            Text("Tip Calculator")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+        }
+        HStack {
+            Text("$")
+            TextField("Amount", text:$total)
+            
+        }
+        HStack{
+            Slider(value: $tipPercent, in: 1...30, step: 1.0)
+            Text("\(Int(tipPercent))")
+            Text("%")
+        }
+        if let totalNum = Double(total) {
+            Text("Tip Amount: $\(totalNum * tipPercent / 100, specifier: "%0.2f")")
+        } else {
+            Text("Please add a numeric value")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tip_calculator/First App/First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Aden Kunvithu Svay on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
