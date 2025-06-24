4f67aeaae2fdb5a1f35310ce8ba1aa47c17f3600
Aden <cambosoccer25@gmail.com>
2025-06-23 23:18:11 -0700

    commenting out code: finishing explainiation for struct and HStacks

--- tip_calculator/FirstApp/ContentView.swift
+++ tip_calculator/FirstApp/ContentView.swift
@@ -10,13 +10,17 @@ import SwiftUI
 
 
 
+// notice how everything is under the struct. Struct has ContentView: View, which at a surface level is everything you can see
 struct ContentView: View {
     
     // @State initializes values, but updates values from the slider
     @State var total = ""
     @State var tipPercent = 15.0
+    
+    // ??
     var body: some View {
         // HStack is a horizontal container
+        // This one is for the dollar sign icon in blue
         HStack {
             Image(systemName: "dollarsign.circle.fill")
                 .imageScale(.small)
@@ -27,16 +31,20 @@ struct ContentView: View {
                 .fontWeight(.bold)
             
         }
+        // This one is for the dollar sign and greyed out text below the previous HStack
         HStack {
             Text("$")
             TextField("Amount", text:$total)
             
         }
+        // For the slider and percent incrementation
         HStack{
             Slider(value: $tipPercent, in: 1...30, step: 1.0)
             Text("\(Int(tipPercent))")
             Text("%")
         }
+        
+        // ??
         if let totalNum = Double(total) {
             Text("Tip Amount: $\(totalNum * tipPercent / 100, specifier: "%0.2f")")
         } else {
@@ -45,6 +53,7 @@ struct ContentView: View {
     }
 }
 
+// This function actually allows our code to be displayed live ->
 #Preview {
     ContentView()
 }
