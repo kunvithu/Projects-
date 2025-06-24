88237c965cebab21e727fd9e9ac25d47db1e322c
Aden <cambosoccer25@gmail.com>
2025-06-23 22:09:11 -0700

    commenting and explaining @state and HStack

--- tip_calculator/FirstApp/ContentView.swift
+++ tip_calculator/FirstApp/ContentView.swift
@@ -3,16 +3,20 @@
 //  First App
 //
 //  Created by Aden Kunvithu Svay on 8/2/24.
-
 // *** see README.md notes about '!' error
 //
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    // @State initializes values, but updates values from the slider
     @State var total = ""
     @State var tipPercent = 15.0
     var body: some View {
+        // HStack is a horizontal container
         HStack {
             Image(systemName: "dollarsign.circle.fill")
                 .imageScale(.small)
