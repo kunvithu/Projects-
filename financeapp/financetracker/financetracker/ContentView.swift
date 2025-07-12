9e7d47f192dc53f765a6af270bddef6bb0f3b26e
Aden <cambosoccer25@gmail.com>
2025-07-11 17:37:33 -0700

    starting project

---
+++ financeapp/financetracker/financetracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  financetracker
+//
+//  Created by Aden Kunvithu Svay on 7/11/25.
+// Start of a new finance tracker app
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ financeapp/financetracker/financetracker/financetrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  financetrackerApp.swift
+//  financetracker
+//
+//  Created by Aden Kunvithu Svay on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct financetrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
